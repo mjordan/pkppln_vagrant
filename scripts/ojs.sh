f5d62825b13a6bed954997ef33142334cb51f8b2
Mark Jordan <mjordan@sfu.ca>
2017-08-03 10:35:35 -0700

    OJS 2.x now installs properly; some additional cleanup of PKP PLN cruft.

--- scripts/ojs.sh
+++ scripts/ojs.sh
@@ -1,5 +1,5 @@
-echo "Installing OJS 2.4.x"
-cd ~
+echo "Installing OJS"
+cd /var/www/html
 
 # Set up the OJS database
 echo "CREATE DATABASE ojs DEFAULT CHARSET utf8" | mysql -uroot -pojs
@@ -7,28 +7,16 @@ echo "CREATE USER 'ojs'@'localhost' IDENTIFIED BY 'ojs'" | mysql -uroot -pojs
 echo "GRANT ALL ON ojs.* TO 'ojs'@'localhost'" | mysql -uroot -pojs
 echo "FLUSH PRIVILEGES" | mysql -uroot -pojs
 
-cd /var/www/html
-
 # Clone the OJS repository
 git clone -b ojs-stable-2_4_8 --single-branch https://github.com/pkp/ojs
 cd ojs
-git submodule update --init --recursive
 cp config.TEMPLATE.inc.php config.inc.php
-mkdir ~/files
-chgrp -R www-data cache public ~/files config.inc.php
-chmod -R ug+w cache public ~/files config.inc.php
+mkdir files
+chgrp -R www-data cache public files config.inc.php
+chmod -R ug+w cache public files config.inc.php
 
-# Install Composer dependencies
+git submodule update --init --recursive
 cd lib/pkp
-curl -sS https://getcomposer.org/installer | php
-php composer.phar update
 
 # Install OJS
-wget -O - --post-data="adminUsername=admin&adminPassword=admin&adminPassword2=admin&adminEmail=ojs@mailinator.com&locale=en_US&additionalLocales[]=en_US&clientCharset=utf-8&connectionCharset=utf8&databaseCharset=utf8&filesDir=%2fhome%2fojs%2ffiles&encryption=sha1&databaseDriver=mysql&databaseHost=localhost&databaseUsername=ojs&databasePassword=ojs&databaseName=ojs&oaiRepositoryId=ojs2.localhost" "http://localhost/ojs/index.php/index/install/install"
-
-mysql -uroot -pojs ojs < /vagrant/ojsdata/ojs2.sql
-
-# Import 
-cd /var/www/html/ojs/tools
-
-php importExport.php NativeImportExportPlugin import /vagrant/ojsdata/test.xml journaloftesting admin
+wget -O - --post-data="adminUsername=admin&adminPassword=ojsadmin&adminPassword2=ojsadmin&adminEmail=ojs@mailinator.com&locale=en_US&additionalLocales[]=en_US&clientCharset=utf-8&connectionCharset=utf8&databaseCharset=utf8&filesDir=%2fvar%2fwww%2fhtml%2fojs%2ffiles&encryption=sha1&databaseDriver=mysql&databaseHost=localhost&databaseUsername=ojs&databasePassword=ojs&databaseName=ojs&oaiRepositoryId=ojs2.localhost" "http://localhost/ojs/index.php/index/install/install"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,18 +13,6 @@ apt-get -y install php5-gd php5-dev php5-xsl php5-curl php5-cli php-pear
 pear install Archive_Tar
 service apache2 restart
 
-# Required by the PKP PLN staging server
-apt-get install -y clamav clamav-daemon
-
 # Install composer
 curl -Ss https://getcomposer.org/installer | php
 mv composer.phar /usr/bin/composer
-
-# Create directories for our applications
-sudo mkdir /var/www/html/ojs
-sudo chown vagrant:vagrant /var/www/html/ojs
-ln -s /var/www/html/ojs /home/vagrant/ojswww
-
-sudo mkdir /var/www/html/pkppln
-sudo chown vagrant:vagrant /var/www/html/pkppln
-ln -s /var/www/html/pkppln /home/vagrant/pkppln
